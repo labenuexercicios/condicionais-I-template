9335587beda635c9612e68386536f97f14b39307
LABNUISA <araujo_isaias@outlook.com>
2022-08-03 22:07:08 -0300

    fixação

---
+++ script.js
@@ -0,0 +1,17 @@
+//FIXAÇÃO
+
+
+let media =prompt("Qual sua nota?")
+
+if (media >=5 ){
+    alert("Parabéns! Você foi aprovado! ")
+}
+else if (media >=3){
+    alert("Você está em recuperação")
+}
+else if (media <3){
+    alert("Você foi reprovado!")
+}
+else {
+    alert("Dado invalido")
+}
