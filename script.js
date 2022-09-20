f0e1ac182be7c94112e321bdc9680377f079ec54
Gabriel Max <gabrielmaxcerqueira@gmail.com>
2022-09-20 16:30:57 -0300

    revisado

---
+++ script.js
@@ -0,0 +1,51 @@
+//  const media = 10
+
+// // media.push(4);
+
+// // if(media >= 5){}
+
+// const bool1 = false
+// const bool2 = true
+// const bool3 = 10
+// // if (bool1){
+// //     alert('Entrou no if. Bool1 é true')
+
+// // }else{
+// //     alert('Entrou no else. Bool1 é false')
+// // }
+
+// // caso ele não leia igual ele vai para o próximo if, e se nao tiver igual, vai para o próximo
+
+// if (bool1 === bool2) {
+//     alert('Bool1 e Bool 2 são iguais.')
+// } else if (bool2 === bool3) {
+//     alert('Bool2 e Bool3 são iguais')
+// } else if (bool1 === bool3) {
+//     alert('Bool1 e Bool3 são iguais')
+// } else {
+//     alert('Não existem Bool iguais')
+// }
+
+
+// let idade = 16
+
+// if(idade >= 60 || idade >= 16 && idade < 18){
+//     console.log('Título facultativo.')
+// }else if(idade >=18){
+//     console.log('Tem direito ao título de eleitor')
+// }else{
+//     console.log("Você não tem a idade mínima para emitir o título")
+// }
+
+
+// let media = 10
+
+// if(media >= 5 && media <= 10){
+//     console.log("Você foi aprovado!")
+// } else if (media === 3 || media === 4) {
+//     console.log("Recuperação")
+// } else if (media < 3) {
+//     console.log("Você foi reprovado!")
+// } else{
+//     console.log('dado inválido')
+// }
