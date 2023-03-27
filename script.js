e58478c90020c297c1bb91ead1dd9c3ca29379ab
rafaelmelodruzian <rmdconsultor@gmail.com>
2023-03-27 15:01:38 -0300

    Exercicios propostos realizados - Condicionais I

---
+++ script.js
@@ -0,0 +1,43 @@
+let booleano1 = false
+let booleano2 = false
+
+// if(booleano1){
+//     alert("Entrei no IF!")
+// }else{
+//     alert("ELSE acessedo!")
+// }
+
+// let booleano3 = true
+// if(booleano1===booleano2){
+//     alert("booleano1 e booleano2 são iguais.")
+// }else if(booleano1===booleano3){
+//     alert("booleano1 e booleano3 são iguais.")
+// }else{
+//     alert("Nem booleano2, nem booleano3 são iguais a booleano1.")
+// }
+
+// let idade =Number (prompt ("Digite sua idade:")) 
+
+// if (18<=idade && idade<=70) {
+//     alert ("Deve votar!")
+// }else if((16<=idade&&idade<18)||idade>70){
+//     alert ("Voto facultativo!")
+// }else{
+//     alert("Não vota!")
+// }
+
+
+// fixação
+
+let media =Number (prompt ("DIGITE A MEDIA FINAL DO ALUNO:"))
+
+if(media>=5&&10>=media){
+    alert("Parabeéns, você foi aprovado(a)!")
+}else if(media>=3&&media<5){
+    alert("Você está de recuperação!")
+}
+    if(media<3){
+   alert("Você foi reprovado :/")
+   }else if(media>10||media<0) {
+    alert("Dado invalido!")
+   }
